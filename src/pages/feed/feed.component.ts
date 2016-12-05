fcd612154e1ffe45084b407a44b7d2d8ceed3bdd
vascosilvaa <vascosilvaa@gmail.com>
2016-12-05 19:50:47 +0000

    feed teste vasco

--- src/pages/feed/feed.component.ts
+++ src/pages/feed/feed.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+import { AngularFire } from 'angularfire2';
 
 import { NavController } from 'ionic-angular';
 
@@ -6,10 +8,20 @@ import { NavController } from 'ionic-angular';
   selector: 'feed',
   templateUrl: 'feed.component.html'
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit{
+  title: string;
+  eventDate: any;
+  date: any;
+  desc: any;
+  type: any;
+  feed: any;
+  codigo: string = "-KYDz-fYCVbIAhjBLMPE";
+  constructor(public navCtrl: NavController, public http:Http, public af: AngularFire) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ngOnInit() {
+      this.feed = this.af.database.list('disciplinas/-KY4Jl3N103L6kNd-liQ/feed');
   }
 
 }
